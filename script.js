96a2c10050d1ae701bf793a332736940ba0c6e51
lukmalone <lukmalone@gmail.com>
2022-02-01 17:15:08 +0200

    Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var burger = document.getElementById("burger");
+var sideNav = document.getElementsByClassName("side-nav")[0];
+var links = document.getElementsByClassName('link');
+
+burger.addEventListener("click", function(){
+  sideNav.classList.toggle("width");
+  this.classList.toggle("rotateZ");
+});
+
+window.addEventListener("resize", function(){
+  if(window.innerWidth > 700){
+    sideNav.classList.remove("width");
+    burger.classList.remove("rotateZ");
+  }
+});
+
+for (var x of links){
+  x.addEventListener("click", function(){
+    for(var x of links){
+      x.classList.remove("active");
+    }
+    this.classList.add("active");
+  });
+}
+
+//SwiperJS
+var buttons = document.getElementsByTagName("button");
+for (var x of buttons){
+  x.addEventListener("click", function(){
+    for(var y of buttons){
+      y.classList.remove("active");
+    }
+    this.classList.add("active");
+    var index = this.getAttribute("index");
+    // var index = document.querySelector(".active").getAttribute("index");
+    swiper.slideTo(index, 1000);
+  });
+}
---
+++ swiper.js
@@ -0,0 +1,76 @@
+"use strict";
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  initialSlide: 0,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  }
+});
+
+var buttons = document.getElementsByName("button");
+swiper.on('slideChange', function () {
+  for(var x of buttons){
+    x.classList.remove("active");
+  }
+  var currentSlide = swiper.activeIndex;
+  var activeButton = buttons[currentSlide];
+  activeButton.classList.add("active");
+});
+
+// Price list
+const swiper2 = new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    1000: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 20,
+      allowSlideNext: false,
+      allowSlidePrev: false
+    }
+  }
+});
+
+
+// reviews
+const swiper3 = new Swiper('.swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    1000: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 15,
+    }
+  }
+});
